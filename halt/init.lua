902c7a28adacc0990169416cd31433b563ed45e6
halttheranger <davidmgang2001@yahoo.com>
2018-01-08 18:23:53 -0500

    Delete init.lua

--- halt/init.lua
+++
@@ -1,9 +0,0 @@
-
-
-minetest.register_node("halt:unbreakable", {
-	description = "unbreakable",
-	tiles = {"halt_unbreakable.png"},
-	sounds = default.node_sound_defaults(),
-	groups = {forbidden=1, very_hard = 1, creative = 1},
-})
-
